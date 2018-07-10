ae3819d517f4f5f31d3a01dd12878d4d06baeb45
Ruslan Dovhospynyi <dovhospynyi@MacBook-Pro-15-Dovgospiniy.local>
2018-07-10 09:58:15 +0300

    PS-1995 rename plugins according interface names

--- src/Spryker/Zed/ProductMeasurementUnitStorage/Communication/Plugin/Event/ProductConcreteMeasurementUnitEventResourceRepositoryPlugin.php
+++ src/Spryker/Zed/ProductMeasurementUnitStorage/Communication/Plugin/Event/ProductConcreteMeasurementUnitEventResourceRepositoryPlugin.php
@@ -7,7 +7,7 @@
 
 namespace Spryker\Zed\ProductMeasurementUnitStorage\Communication\Plugin\Event;
 
-use Orm\Zed\ProductMeasurementUnit\Persistence\Map\SpyProductMeasurementUnitTableMap;
+use Orm\Zed\ProductMeasurementUnit\Persistence\Map\SpyProductMeasurementSalesUnitTableMap;
 use Spryker\Shared\ProductMeasurementUnitStorage\ProductMeasurementUnitStorageConfig;
 use Spryker\Zed\EventBehavior\Dependency\Plugin\EventResourceRepositoryPluginInterface;
 use Spryker\Zed\Kernel\Communication\AbstractPlugin;
@@ -18,7 +18,7 @@ use Spryker\Zed\ProductMeasurementUnit\Dependency\ProductMeasurementUnitEvents;
  * @method \Spryker\Zed\ProductMeasurementUnitStorage\Business\ProductMeasurementUnitStorageFacadeInterface getFacade()
  * @method \Spryker\Zed\ProductMeasurementUnitStorage\Communication\ProductMeasurementUnitStorageCommunicationFactory getFactory()
  */
-class ProductMeasurementUnitEventResourcePlugin extends AbstractPlugin implements EventResourceRepositoryPluginInterface
+class ProductConcreteMeasurementUnitEventResourceRepositoryPlugin extends AbstractPlugin implements EventResourceRepositoryPluginInterface
 {
     /**
      * Specification:
@@ -30,7 +30,7 @@ class ProductMeasurementUnitEventResourcePlugin extends AbstractPlugin implement
      */
     public function getResourceName(): string
     {
-        return ProductMeasurementUnitStorageConfig::PRODUCT_MEASUREMENT_UNIT_RESOURCE_NAME;
+        return ProductMeasurementUnitStorageConfig::PRODUCT_CONCRETE_MEASUREMENT_UNIT_RESOURCE_NAME;
     }
 
     /**
@@ -47,9 +47,10 @@ class ProductMeasurementUnitEventResourcePlugin extends AbstractPlugin implement
     public function getData(array $ids = []): array
     {
         if (!empty($ids)) {
-            return $this->getFacade()->findAllProductMeasurementUnitTransfers();
+            return $this->getFacade()->getSalesUnitsByIds($ids);
         }
-        return $this->getFacade()->findProductMeasurementUnitTransfers($ids);
+
+        return $this->getFacade()->getSalesUnits();
     }
 
     /**
@@ -62,7 +63,7 @@ class ProductMeasurementUnitEventResourcePlugin extends AbstractPlugin implement
      */
     public function getEventName(): string
     {
-        return ProductMeasurementUnitEvents::PRODUCT_MEASUREMENT_UNIT_PUBLISH;
+        return ProductMeasurementUnitEvents::PRODUCT_CONCRETE_MEASUREMENT_UNIT_PUBLISH;
     }
 
     /**
@@ -75,6 +76,6 @@ class ProductMeasurementUnitEventResourcePlugin extends AbstractPlugin implement
      */
     public function getIdColumnName(): string
     {
-        return SpyProductMeasurementUnitTableMap::COL_ID_PRODUCT_MEASUREMENT_UNIT;
+        return SpyProductMeasurementSalesUnitTableMap::COL_FK_PRODUCT;
     }
 }
