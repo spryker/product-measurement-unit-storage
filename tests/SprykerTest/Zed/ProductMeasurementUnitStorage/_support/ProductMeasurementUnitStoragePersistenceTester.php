91a3e35211cf586994f8f3d6fb4f61b2e7e51963
René Klatt <rene.klatt@spryker.com>
2021-05-27 12:33:36 +0200

    TE-8573 P&S Testing Infrastructure (#8130)
    TE-8573 P&S Testing Infrastructure

--- tests/SprykerTest/Zed/ProductMeasurementUnitStorage/_support/ProductMeasurementUnitStoragePersistenceTester.php
+++ tests/SprykerTest/Zed/ProductMeasurementUnitStorage/_support/ProductMeasurementUnitStoragePersistenceTester.php
@@ -10,6 +10,8 @@ namespace SprykerTest\Zed\ProductMeasurementUnitStorage;
 use Codeception\Actor;
 
 /**
+ * Inherited Methods
+ *
  * @method void wantToTest($text)
  * @method void wantTo($text)
  * @method void execute($callable)
@@ -19,11 +21,11 @@ use Codeception\Actor;
  * @method void am($role)
  * @method void lookForwardTo($achieveValue)
  * @method void comment($description)
- * @method \Codeception\Lib\Friend haveFriend($name, $actorClass = null)
+ * @method void pause()
  *
  * @SuppressWarnings(PHPMD)
  */
-class ProductMeasurementUnitStorageBusinessTester extends Actor
+class ProductMeasurementUnitStoragePersistenceTester extends Actor
 {
-    use _generated\ProductMeasurementUnitStorageBusinessTesterActions;
+    use _generated\ProductMeasurementUnitStoragePersistenceTesterActions;
 }
